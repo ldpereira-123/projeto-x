c2703091babf72fb24e8ac625cdd88e7ff3e1f62
Leandro de Matos Pereira <ldpereira@uolinc.com>
2019-05-02 15:39:52 -0300

    revisando código

--- write_on_file.py
+++ write_on_file.py
@@ -4,4 +4,4 @@ now = time.ctime()
 
 def write(files):
     with open('espelho_log.txt', 'a') as escreve:
-        escreve.write('{}: Existem mais de 3 arquivos: {}\n'.format(now, files))		
+        escreve.write('{}: Existem + de 3 arquivos: {}\n'.format(now, files))		
